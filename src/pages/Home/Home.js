12ac3c5d139f2d76f19caa244846d67c4d893f74
CTLong02 <chuthienlong02@gmail.com>
2023-06-11 10:36:38 +0700

    UI homePage and footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,33 @@
-import axios from 'axios';
+import clsx from 'clsx';
+import styles from './Home.module.scss';
+import { Fade } from 'react-reveal';
+import Footer from '~/app/components/Footer';
+import imgBanner from '~/assets/images/img_banner.png';
 function Home() {
-    return <div></div>;
+    return (
+        <div className={styles.banner}>
+            <div className="container-xl d-flex align-items-center" style={{ minHeight: 600 }}>
+                <Fade right>
+                    <div className="col-md-6 col-12">
+                        <h3>Chào mừng các bạn đến với thư viện số</h3>
+                        <div>
+                            <h3>Thư viện số</h3>
+                            <p>
+                                Thư viện số đươc xây dựng dựa trên nguồn tài liệu nội sinh của trường. Các nguồn bài
+                                giảng giáo trình do thầy cô là giảng viên của trường biên soạn. Ngoài ra còn có bộ sưu
+                                tập luận văn của sinh viên trường đóng góp. Để sử dụng nguồn tài liệu nội sinh - Độc giả
+                                cần có tài khoản sử dụng Thư viện số được cung cấp bởi thư viện.
+                            </p>
+                        </div>
+                    </div>
+                </Fade>
+                <div className="col-md-6 col-12 d-flex justify-content-center align-items-end">
+                    <img src={imgBanner}></img>
+                </div>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default Home;
